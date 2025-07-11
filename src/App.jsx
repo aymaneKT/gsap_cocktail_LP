5e5dc202b2c9d17bb1f04036f5b54ff72d85477c
aymaneKT <aymanekabtitech@gmail.com>
2025-07-11 12:37:51 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import "./App.css";
+gsap.registerPlugin(ScrollTrigger, SplitText);
+function App() {
+  return <></>;
+}
+
+export default App;
